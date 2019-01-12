f7187f719eb102ffae8a96827fb832035b4e41cd
mjamry <michal.jamry@gmail.com>
2019-01-12 12:40:37 +0100

    Resolved an issue with incorrect classes in item.

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -6,10 +6,10 @@ class Item extends Component {
   render(){
     return (
       <div className="Item row">
-        <div className="item-position col-1" class="col-1"></div>
-        <div className="item-name" class="col-3"><a href={this.props.value.link}>{this.props.value.name}</a></div>
-        <div className="item-description" class="col-6">{this.props.value.description}</div>
-        <div className="item-attractiveness" class="col-2">{this.props.value.attractivness}</div>
+        <div className="item-position col-1"></div>
+        <div className="item-name col-3"><a href={this.props.value.link}>{this.props.value.name}</a></div>
+        <div className="item-description col-6">{this.props.value.description}</div>
+        <div className="item-attractiveness col-2">{this.props.value.attractivness}</div>
       </div>
     )
   }
