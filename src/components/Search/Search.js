ff4e53191d7116c3ca9194e68b8e2397ee7d23bb
mjamry <michal.jamry@gmail.com>
2019-01-24 11:33:51 +0100

    Extended search component functionality.

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,36 @@
 import React, {Component} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+const SearchTimeout = 700;
+
 class Search extends Component {
+  constructor(props){
+    super(props);
+
+    this.onChange = this.onChange.bind(this);
+    this.setupTimer = this.setupTimer.bind(this);
+    this.handleSearchInputTimeout = this.handleSearchInputTimeout.bind(this);
+
+    this.state = {
+      searchValue: "",
+      timer: null
+    }
+  }
+
+  onChange(e){
+    this.setState({searchValue: e.target.value});
+    this.setupTimer();
+  }
+
+  setupTimer(){
+    clearTimeout(this.state.timer);
+    this.setState({timer: setTimeout(this.handleSearchInputTimeout, SearchTimeout)})
+  }
+
+  handleSearchInputTimeout(){
+    console.log(this.state.searchValue);
+  }
+
   render(){
     return (
       <div className="Search">
@@ -9,7 +38,7 @@ class Search extends Component {
           <div class="input-group-prepend">
            <span class="input-group-text"><FontAwesomeIcon icon="search-location" className="fa-2x"/></span>
         </div>
-        <input type="text" class="form-control" placeholder="Username"/>
+        <input type="text" class="form-control" placeholder="Search location" onChange={this.onChange}/>
       </div>
       </div>
     )
