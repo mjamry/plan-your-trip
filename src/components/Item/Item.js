57c2b5e027ca1affbd54613f825b34d2dc319485
mjamry <michal.jamry@gmail.com>
2019-01-25 23:12:08 +0100

    Added image to the item on the list.

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,7 +4,7 @@ class Item extends Component {
   render(){
     return (
       <div className="Item row">
-        <div className="item-position col-1"></div>
+        <div className="col-1"><img className="item-image" src={this.props.value.image} alt=''></img></div>
         <div className="item-name col-3" title={this.props.value.link}><a href={this.props.value.link} target="_blank" rel="noopener noreferrer">{this.props.value.name}</a></div>
         <div className="item-description col-6" title={this.props.value.description}>{this.props.value.description}</div>
         <div className="item-attractiveness col-2">{this.props.value.attractivness}</div>
