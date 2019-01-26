c65488bf4eb6857d3a46d47ec4c59bfbe15a0d29
mjamry <michal.jamry@gmail.com>
2019-01-26 11:19:56 +0100

    Added possibility to remove items from the list.

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Item extends Component {
   render(){
     return (
       <div className="Item row">
-        <div className="col-1"><img className="item-image" src={this.props.value.image} alt=''></img></div>
-        <div className="item-name col-3" title={this.props.value.link}><a href={this.props.value.link} target="_blank" rel="noopener noreferrer">{this.props.value.name}</a></div>
-        <div className="item-description col-6" title={this.props.value.description}>{this.props.value.description}</div>
-        <div className="item-attractiveness col-2">{this.props.value.attractivness}</div>
+        <div className="row col-2">
+          <img className="item-image" src={this.props.value.image} alt={this.props.value.name}></img>
+        </div>
+        <div className="item-details row col-10">
+          <div className="item-name col-2" title={this.props.value.link}><a href={this.props.value.link} target="_blank" rel="noopener noreferrer">{this.props.value.name}</a></div>
+          <div className="item-description col-8" title={this.props.value.description}>{this.props.value.description}</div>
+          <div className="row col-2">
+            <div className="item-attractiveness col-4">{this.props.value.attractivness}</div>
+            <div className="col-8">
+              <FontAwesomeIcon icon="trash-alt" title="remove item" className="item-delete fa-2x" onClick={()=>this.props.onRemoved(this.props.index)}/>
+            </div>
+          </div>
+        </div>
+               
       </div>
     )
   }
--- src/components/ItemsList/ItemsList.js
+++ src/components/ItemsList/ItemsList.js
@@ -6,10 +6,14 @@ class ItemsList extends Component {
     var output = [];
     for(var item in this.props.list)
     {
-      output.push(<Item value={this.props.list[item]}></Item>)
+      output.push(<Item value={this.props.list[item]} onRemoved={this.handleItemRemoved} key={item} index={item}></Item>)
     }
 
     return output;
+  }
+
+  handleItemRemoved = (index) => {
+    this.props.onRemoved(index);
   }
 
   render(){
