ae178cc752afb772ce4643359e30f2035b043d23
mjamry <michal.jamry@gmail.com>
2019-11-10 14:33:20 +0100

    Added possibility to change wiki search engine. Get users default language from browser settings.

--- src/Common/WikipediaAPIWrapper.js
+++ src/Common/WikipediaAPIWrapper.js
@@ -1,19 +1,21 @@
 import wiki from 'wikijs';
 import ItemDetails from '../ItemDetails';
+import LanguageProvider from '../Common/LanguageProvider';
 
 const NumberOfResults = 5;
+const API_URL = `https://${LanguageProvider.get()}.wikipedia.org/w/api.php`
 
 export default class WikipediaAPIWrapper{
     static searchResult = {}
 
     static search(input){
-        return wiki()
+        return wiki({apiUrl: API_URL})
         .search(input)
         .then(data => data.results.splice(0, NumberOfResults))
     }
 
     static getDetails(input){
-        return wiki()
+        return wiki({apiUrl: API_URL})
         .page(input)
         .then(page => {return Promise.all([page.info("name"), page.summary(), page.coordinates(), page.mainImage()])})
         .then(([name, details, geo, img] )=> new ItemDetails(name, details, 0, geo, img, ""))
