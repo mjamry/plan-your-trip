b3aec49fa95c7e924edb5e6122567ef216523b70
mjamry <mjamry@future-processing.com>
2019-11-19 13:04:57 +0100

    First part of changes for simpliests components.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Search from './Search/Search'
 
-class Header extends Component {
-  render(){
-    return (
-      <div className="Header">
-        <nav class="navbar">
-            <div><FontAwesomeIcon icon='globe-americas'/> Plan the Trip</div>
-            <div>
-              <Search onFinished={this.props.onSearchFinished}/>
-            </div>
-        </nav>
-      </div>
-    )
-  }
+var Header = (props) => {
+  return (
+    <div className="Header">
+      <nav class="navbar">
+          <div><FontAwesomeIcon icon='globe-americas'/> Plan the Trip</div>
+          <div>
+            <Search onFinished={props.onSearchFinished}/>
+          </div>
+      </nav>
+    </div>
+  )
 }
 
 export default Header;
--- src/components/Items/GridItem.js
+++ src/components/Items/GridItem.js
@@ -1,27 +1,25 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-class GridItem extends Component {
-  render(){
-    return (
-      <div className="GridItem">
-        <div class="card">
-          <img className="grid-item-image" src={this.props.value.image} alt={this.props.value.name}></img>
-          <div className="card-body">
-            <h5 className="card-title grid-item-name" onClick={()=>this.props.onSelected(this.props.index)}>{this.props.value.name}</h5>
-            <p className="card-text grid-item-description" title={this.props.value.description}>{this.props.value.description}</p>
-            <hr></hr>
-            <div className="row container">
-              <p className="col-8 grid-item-coordinates"><FontAwesomeIcon icon="map-marker"/> 43°41′S 170°10′E</p>
-              <p className="col-2">{this.props.value.attractivness}</p>
-              <p className="col-2 grid-item-remove"><FontAwesomeIcon icon="trash-alt" title="remove item" onClick={()=>this.props.onRemoved(this.props.index)}/></p>
-            </div>
+var GridItem = (props) => {
+  return (
+    <div className="GridItem">
+      <div class="card">
+        <img className="grid-item-image" src={props.value.image} alt={props.value.name}></img>
+        <div className="card-body">
+          <h5 className="card-title grid-item-name" onClick={()=>props.onSelected(props.index)}>{props.value.name}</h5>
+          <p className="card-text grid-item-description" title={props.value.description}>{props.value.description}</p>
+          <hr></hr>
+          <div className="row container">
+            <p className="col-8 grid-item-coordinates"><FontAwesomeIcon icon="map-marker"/> 43°41′S 170°10′E</p>
+            <p className="col-2">{props.value.attractivness}</p>
+            <p className="col-2 grid-item-remove"><FontAwesomeIcon icon="trash-alt" title="remove item" onClick={()=>props.onRemoved(props.index)}/></p>
           </div>
-          
         </div>
+        
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default GridItem;
