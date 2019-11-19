3ce27fc6c75b1f2f1ed1a7ef9bfec2ab692ab82a
mjamry <mjamry@future-processing.com>
2019-11-19 13:20:36 +0100

    Updated search result component.

--- src/components/Search/SearchResult.js
+++ src/components/Search/SearchResult.js
@@ -1,37 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class SearchResult extends Component {
+var SearchResult = (props) => {
 
-  renderResults(){  
+  var renderResults = () => {  
     let output = [];
-    for(let index in this.props.results){
+    for(let index in props.results){
       output.push(
       <div 
         className="dropdown-item search-result" 
         data-toggle="modal" data-target="#itemDetailsFormModal"
-        onClick={()=>this.handleSelection(this.props.results[index])}>
-        {this.props.results[index]}
+        onClick={()=>props.onSelected(props.results[index])}>
+        {props.results[index]}
       </div>)
     }
 
     return output;
   }
 
-  handleSelection(selection){
-    this.props.onSelected(selection)
-  }
-
-  render(){
-    return (
-      <div className="SearchResult">
-        <div className={this.props.isOpened + " dropdown"}>
-          <div className={this.props.isOpened + " dropdown-menu"} aria-labelledby="dropdownMenuButton">
-            {this.renderResults()}
-          </div>
+  return (
+    <div className="SearchResult">
+      <div className={props.isOpened + " dropdown"}>
+        <div className={props.isOpened + " dropdown-menu"} aria-labelledby="dropdownMenuButton">
+          {renderResults()}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default SearchResult;
