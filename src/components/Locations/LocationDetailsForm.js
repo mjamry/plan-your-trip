24679a30bfb1842a48a03070a45f96b76d94b6ca
mjamry <michal.jamry@gmail.com>
2019-11-20 19:44:11 +0100

    Updated LocationDetailsForm to use hooks.

--- src/components/Locations/LocationDetailsForm.js
+++ src/components/Locations/LocationDetailsForm.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import Modal from '../../Common/Modal';
 
 const EMPTY_ITEM_DATA = {
@@ -7,93 +7,79 @@ const EMPTY_ITEM_DATA = {
     coordinates: {lat: "", lon: ""}
 }
 
-class LocationDetailsForm extends Component{
-    constructor(props){
-        super(props);
-        this.handleFormChanged = this.handleFormChanged.bind(this);
-        this.handleGpsFormChanged = this.handleGpsFormChanged.bind(this);
-        this.clearData = this.clearData.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
+var LocationDetailsForm = (props) => {
+    const [location, setLocation] = useState(EMPTY_ITEM_DATA);
+    const [dataReady, setDataReady] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
-        this.state = {
-            item: EMPTY_ITEM_DATA,
-            dataReady: false,
-            show: false
+    useEffect(()=>{
+        if(props.item !== null)
+        {
+            setLocation(props.item);
+            setDataReady(true);
+            setShowModal(true);
         }
-    }
+    }, [props.item])
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.item !== this.props.item && this.props.item !== null) {
-            this.setState({
-                item: this.props.item,
-                dataReady: true,
-                show: true
-            })
-        }
-    }
-
-    handleFormChanged(e){
+    var handleFormChanged = (e) => {
         const name = e.target.name;
         const value = e.target.value;
-        this.setState({
-            item:{
-                ...this.state.item, 
+        setLocation(
+            {
+                ...location, 
                 [name]: value
-            }
-        });
+            });
     }
 
-    handleGpsFormChanged(e){
+    var handleGpsFormChanged = (e) => {
         const name = e.target.name;
         const value = e.target.value;
-        this.setState({
-            item: {
-                ...this.state.item, 
+        setLocation(
+            {
+                ...location,
                 coordinates: {
-                    ...this.state.item.coordinates,
+                    ...location.coordinates,
                     [name]: value
                 }
             }
-        });
+        )
     }
 
-    onSubmit(item){
-        this.props.onFinished(this.state.item);
-        this.clearData()
+    var onSubmit = () => {
+        props.onFinished(location);
+        clearData()
     }
 
-    clearData(){
-        this.setState({
-            item: EMPTY_ITEM_DATA,
-            dataReady: false,
-            show: false
-        })
+    var clearData = () => {
+        setLocation(EMPTY_ITEM_DATA);
+        setDataReady(false);
+        setShowModal(false);
     }
 
-    renderForm(){
+    var renderForm = () => {
         return(<form>
             <div className="form-group">
                 <label htmlFor="item-name" className="col-form-label">Name</label>
-                <input name="name" onChange={this.handleFormChanged} className="form-control" id="item-name" value={this.state.item.name}/>
+                <input name="name" onChange={handleFormChanged} className="form-control" id="item-name" value={location.name}/>
             </div>
             <div className="form-row">
             <div className="form-group col-md-6">
                 <label htmlFor="item-coordinates-lat" className="col-form-label">Gps latitude</label>
-                <input name="lat" onChange={this.handleGpsFormChanged} className="form-control" id="item-coordinates-lat" value={this.state.item.coordinates.lat}/>
+                <input name="lat" onChange={handleGpsFormChanged} className="form-control" id="item-coordinates-lat" value={location.coordinates.lat}/>
             </div>
             <div className="form-group col-md-6">
                 <label htmlFor="item-coordinates-lon" className="col-form-label">Gps longitude</label>
-                <input name="lon" onChange={this.handleGpsFormChanged} className="form-control" id="item-coordinates-lon" value={this.state.item.coordinates.lon}/>
+                <input name="lon" onChange={handleGpsFormChanged} className="form-control" id="item-coordinates-lon" value={location.coordinates.lon}/>
             </div>
             </div>
             <div className="form-group">
                 <label htmlFor="item-description">Description</label>
-                <textarea name="description" onChange={this.handleFormChanged} className="form-control" rows="5" id="item-description" value={this.state.item.description}></textarea>
+                <textarea name="description" onChange={handleFormChanged} className="form-control" rows="5" id="item-description" value={location.description}></textarea>
             </div>
         </form>)
     }
 
-    renderLoader(){
+    var renderLoader = () => {
         return(
             <div className="d-flex justify-content-center">
                 <span>Loading...</span>
@@ -101,26 +87,24 @@ class LocationDetailsForm extends Component{
         );
     }
 
-    renderFooter(){
+    var renderFooter = ()  => {
         return(
             <div>
-                <button type="submit" className="btn btn-primary" onClick={this.onSubmit} data-dismiss="modal">Save</button>
-                <button type="button" className="btn" data-dismiss="modal" onClick={this.clearData}>Cancel</button>
+                <button type="submit" className="btn btn-primary" onClick={onSubmit} data-dismiss="modal">Save</button>
+                <button type="button" className="btn" data-dismiss="modal" onClick={clearData}>Cancel</button>
             </div>
         );
     }
 
-    render(){
-        return(
-            <div>
-                <Modal 
-                    show={this.state.show} 
-                    content={this.state.dataReady ? this.renderForm() : this.renderLoader()}
-                    header="Edit details" 
-                    footer={this.renderFooter()}/>
-            </div>
-        )
-    }
+    return(
+        <div>
+            <Modal 
+                show={showModal} 
+                content={dataReady ? renderForm() : renderLoader()}
+                header="Edit details" 
+                footer={renderFooter()}/>
+        </div>
+    )
 };
 
 export default LocationDetailsForm;
