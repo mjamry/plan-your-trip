3b77dc11e78866980158dc04a418145593b9d672
mjamry <mjamry@future-processing.com>
2019-12-19 14:52:35 +0100

    Improved row view for locations list

--- src/components/Locations/LocationListItem.js
+++ src/components/Locations/LocationListItem.js
@@ -3,32 +3,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 var LocationListItem = (props) => {
   return (
-    <div className="Listlocation row">
-      <div className="row col-2">
-        <img className="list-location-image" src={props.location.image} alt={props.location.name}></img>
+    <div className="location-row-container">
+      <img className="location-row-image" src={props.location.image} alt={props.location.name}></img>
+      <div className="location-row-name" 
+        title={props.location.link} 
+        onClick={props.onSelect}>{props.location.name}</div>
+      <div className="location-row-description" title={props.location.description}>{props.location.description}</div>
+      <div className="location-row-attractivness">{props.location.attractivness}</div>
+      <div className="location-row-buttons">
+      <FontAwesomeIcon 
+        icon="trash-alt" 
+        title="remove location" 
+        className="location-row-button" 
+        onClick={props.onRemove}/>
+      <FontAwesomeIcon 
+        icon="edit" 
+        title="edit location" 
+        className="location-row-button" 
+        onClick={props.onEdit}/>
       </div>
-      <div className="list-location-details row col-10">
-        <div className="list-location-name col-2" 
-          title={props.location.link} 
-          onClick={props.onSelect}>{props.location.name}</div>
-        <div className="list-location-description col-8" title={props.location.description}>{props.location.description}</div>
-        <div className="row col-2">
-          <div className="list-location-attractiveness col-4">{props.location.attractivness}</div>
-          <div className="col-8">
-            <FontAwesomeIcon 
-              icon="trash-alt" 
-              title="remove location" 
-              className="list-location-delete fa-2x" 
-              onClick={props.onRemove}/>
-            <FontAwesomeIcon 
-              icon="edit" 
-              title="edit location" 
-              className="list-location-delete fa-2x" 
-              onClick={props.onEdit}/>
-          </div>
-        </div>
-      </div>
-    </div>
+  </div>
   )
 }
 
