27fcbbb0c31cf9e0f50ba721302a1613297fb23c
mjamry <mjamry@future-processing.com>
2020-01-09 16:44:27 +0100

    modified location DTO id value

--- src/Common/WikipediaAPIWrapper.js
+++ src/Common/WikipediaAPIWrapper.js
@@ -18,6 +18,6 @@ export default class WikipediaAPIWrapper{
         return wiki({apiUrl: API_URL})
         .page(input)
         .then(page => {return Promise.all([page.raw.title, page.summary(), page.coordinates(), page.mainImage()])})
-        .then(([name, details, geo, img] )=> new locationDetails(name, details, 0, geo, img, ""))
+        .then(([name, details, geo, img] )=> new locationDetails(0, name, details, 0, geo, img, ""))
     }
 }
--- src/components/Locations/LocationdDetailsDto.js
+++ src/components/Locations/LocationdDetailsDto.js
@@ -1,12 +1,12 @@
 export default class locationDetails {
-  constructor(name, description, attractivness, coordinates, image, link){
+  constructor(id, name, description, attractivness, coordinates, image, link){
     this.name = name;
     this.description = description;
     this.attractivness = attractivness || 1;
     this.coordinates = coordinates;
     this.image = image;
     this.link = link;
-    this.id = (+new Date()).toString(32);
+    this.id = id;
   }
     id;
     name;
