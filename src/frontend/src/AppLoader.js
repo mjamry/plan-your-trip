0b102bbca29a2212d363df5f1ad48f9aad5b86b2
mjamry <michal.jamry@gmail.com>
2020-06-09 13:30:33 +0200

    Added background image to the app loader

--- src/frontend/src/AppLoader.js
+++ src/frontend/src/AppLoader.js
@@ -12,6 +12,7 @@ const NUMBER_OF_STEPS = 4;
 
 const AppLoader = () => {
     const [progress, setProgress] = useState(0);
+    const [progressDetails, setProgressDetails] = useState("");
     const [isLoading, setIsLoading] = useState(true);
     const [{selectedListId}, dispatchLists] = useListsState();
     const locationService = useLocationsService();
@@ -32,21 +33,25 @@ const AppLoader = () => {
             setIsLoading(true);
             //get USER
             setProgress(1/NUMBER_OF_STEPS);
+            setProgressDetails("Loading user data");
             log.debug("Get User");
             const user = await userService.getUser();
 
             setProgress(2/NUMBER_OF_STEPS);
+            setProgressDetails("Loading user lists");
             log.debug("Get Lists");
             let lists = await listService.getAll();
 
             if(lists.length > 0)
             {
                 setProgress(3/NUMBER_OF_STEPS);
+                setProgressDetails("Loading user locations");
                 log.debug("Get locations");
                 await locationService.getAll(lists[0].id);
             }
 
             setProgress(4/NUMBER_OF_STEPS);
+            setProgressDetails("Finalize");
             setIsLoading(false);
         };
 
@@ -56,13 +61,18 @@ const AppLoader = () => {
 
     return (
     <>
-    { isLoading &&  <div className="app-loader-container">
-            <div className="app-loader-title">
-                <div className="app-loader-progress">
-                    <div>progress: {Math.round(progress*100)}%</div>
-                    <div><FontAwesomeIcon icon="spinner" spin className="fa-2x" /></div>
+    { isLoading && <div> 
+            <div className="app-loader-background"></div>
+            <div className="app-loader-container">
+                <div className="app-loader-box">
+                    <div className="app-loader-logo">Trip Planner Logo</div>
+                    <div className="app-loader-progress">
+                        <span>loading progress: {Math.round(progress*100)}%</span>
+                        <span><FontAwesomeIcon icon="spinner" spin className="fa-2x" /></span>
+                        <span className="app-loader-details">{progressDetails}</span>
+                    </div>
+                    <div className="app-container-footer"></div>
                 </div>
-                <div className="app-container-footer"></div>
             </div>
         </div>}
     </>);
