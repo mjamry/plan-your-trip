271f35ab0f8ca6d8bd823ed630ceabca25387228
mjamry <michal.jamry@gmail.com>
2021-11-19 14:02:09 +0100

    #85 Added possibility to get users location

--- src/TripPlanner.App/ClientApp/src/components/modals/LocationDetailsForm/Steps/LocationCoordinatesStep.tsx
+++ src/TripPlanner.App/ClientApp/src/components/modals/LocationDetailsForm/Steps/LocationCoordinatesStep.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
 import L from 'leaflet';
+import { Button } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import LocationFormMapView from '../../../MapView/LocationFormMapView';
 import useCoordinatesValidator from '../../../../Common/CoordinatesValidator';
 import { useLocationFormState, LocationFormStateActions } from '../LocationDetailsFormState';
 import { IStepValidator } from './Step';
 import LocationDto from '../../../../Common/Dto/LocationDto';
+import useLocationProvider from '../../../../Services/LocationProvider';
+
+const useStyles = makeStyles({
+  getLocationButton: {
+    textAlign: 'center',
+  },
+  formRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignContent: 'flex-start',
+    margin: '5px 0 10px 0',
+  },
+  formItem: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: '10px',
+    margin: '0 5px 0 5px',
+  },
+});
 
 export const LocationCoordinatesStep = () => {
+  const classes = useStyles();
   const { state, dispatch } = useLocationFormState();
   const coordinatesValidator = useCoordinatesValidator();
+  const locationProvider = useLocationProvider();
 
   const handleCoordinatesChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (coordinatesValidator.isValid(e.target.value)) {
@@ -41,10 +65,28 @@ export const LocationCoordinatesStep = () => {
     });
   };
 
+  const handleGetLocation = async () => {
+    const userLocation = await locationProvider.getLocation();
+    if (coordinatesValidator.isValid(userLocation.lat)
+      && coordinatesValidator.isValid(userLocation.lon)) {
+      dispatch({
+        type: LocationFormStateActions.updateLocation,
+        data:
+          {
+            ...state.location,
+            coordinates: {
+              lat: userLocation.lat,
+              lon: userLocation.lon,
+            },
+          },
+      });
+    }
+  };
+
   return (
     <form>
-      <div className="location-edit-form-row">
-        <div className="location-edit-form-item">
+      <div className={classes.formRow}>
+        <div className={classes.formItem}>
           <TextField
             name="lat"
             label="Latitude"
@@ -53,11 +95,10 @@ export const LocationCoordinatesStep = () => {
             margin="dense"
             onChange={handleCoordinatesChanged}
             value={state.location?.coordinates.lat || ''}
-            helperText="test"
           />
         </div>
 
-        <div className="location-edit-form-item">
+        <div className={classes.formItem}>
           <TextField
             name="lon"
             label="Longitude"
@@ -70,7 +111,19 @@ export const LocationCoordinatesStep = () => {
         </div>
       </div>
 
-      <div className="location-edit-form-row">
+      {locationProvider.canGetLocation && (
+        <div className={classes.formRow}>
+          <Button
+            onClick={handleGetLocation}
+            size="small"
+            variant="contained"
+          >
+            Get my location
+          </Button>
+        </div>
+      )}
+
+      <div className={classes.formRow}>
         <LocationFormMapView
           location={state.location}
           onCoordinatesUpdated={handleMapCoordinatesChanged}
