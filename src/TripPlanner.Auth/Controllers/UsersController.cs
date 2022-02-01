922bc01aded1483846283b7d5328e5680ba4d4b3
mjamry <michal.jamry@gmail.com>
2022-02-01 13:48:07 +0100

    #58 Added api to the auth project to expose users details.

---
+++ src/TripPlanner.Auth/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TripPlanner.Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    // TODO do not allow to access from other clients
+    // [Authorize]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UsersController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IEnumerable<UserDto> GetUsers()
+        {
+            var users = _userManager.Users
+                .Where(u => u.EmailConfirmed == true)
+                .Select(u => new UserDto() { Id = u.Id, Name = u.UserName, Email = u.Email });
+
+            return users;
+        }
+    }
+}
