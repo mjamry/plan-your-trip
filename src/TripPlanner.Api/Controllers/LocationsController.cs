a5025f2f03527d409b5b57659dffa017227564db
mjamry <michal.jamry@gmail.com>
2022-02-02 14:10:28 +0100

    #58 Plan details are now available only for user that are assigned to the plan (owner, or shared)

--- src/TripPlanner.Api/Controllers/LocationsController.cs
+++ src/TripPlanner.Api/Controllers/LocationsController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using trip_planner.Data;
 using trip_planner.Data.Models;
+using TripPlanner.Api.Common;
 
 namespace trip_planner.Controllers
 {
@@ -10,18 +12,29 @@ namespace trip_planner.Controllers
     [Authorize]
     public class LocationsController : ControllerBase
     {
-        private ILocationsRepository _repo;
+        private ILocationsRepository _locationsRepository;
+        private readonly IPlanRepository _planRepository;
+        private readonly ICurrentUser _currentUser;
 
-        public LocationsController(ILocationsRepository repo)
+        public LocationsController(ILocationsRepository locationsRepository, IPlanRepository planRepository, ICurrentUser currentUser)
         {
-            _repo = repo;
+            _locationsRepository = locationsRepository;
+            _planRepository = planRepository;
+            _currentUser = currentUser;
         }
 
         [HttpGet]
         [Route("{planId}")]
         public IActionResult Get([FromRoute] int planId)
         {
-            var locations = _repo.GetLocations(planId);
+            var userPlans = _planRepository.GetPlans(_currentUser.Id).Where(p => p.Id == planId);
+
+            if (userPlans.Count() == 0)
+            {
+                return NotFound($"There is no plan with specified ID: {planId}");
+            }
+
+            var locations = _locationsRepository.GetLocations(planId);
 
             return Ok(locations);
         }
@@ -30,7 +43,14 @@ namespace trip_planner.Controllers
         [Route("{planId}")]
         public IActionResult Create([FromBody] Location location, [FromRoute] int planId)
         {
-            _repo.CreateLocation(location, planId);
+            var userPlans = _planRepository.GetPlans(_currentUser.Id).Where(p => p.Id == planId);
+
+            if (userPlans.Count() == 0)
+            {
+                return NotFound($"There is no plan with specified ID: {planId}");
+            }
+
+            _locationsRepository.CreateLocation(location, planId);
 
             return Created(string.Empty, location);
         }
@@ -38,12 +58,14 @@ namespace trip_planner.Controllers
         [HttpPut]
         public IActionResult Update([FromBody] Location location)
         {
-            var result = _repo.UpdateLocation(location);
+            var loc = _locationsRepository.GetLocation(location.Id);
 
-            if (result == null)
+            if(loc == null)
             {
                 return NotFound($"There is no location with specified ID: {location.Id}");
             }
+
+            var result = _locationsRepository.UpdateLocation(location);
 
             return Ok(location);
         }
@@ -51,7 +73,7 @@ namespace trip_planner.Controllers
         [HttpDelete]
         public IActionResult Delete([FromBody] Location location)
         {
-            var result = _repo.DeleteLocation(location);
+            var result = _locationsRepository.DeleteLocation(location);
 
             if (result == null)
             {
