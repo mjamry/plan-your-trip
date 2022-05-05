744f2c109553f00381c6e0f74950feb4b195140b
mjamry <michal.jamry@gmail.com>
2022-05-05 17:40:19 +0200

    #121 Updated image step to show loading indicator

--- src/TripPlanner.App/ClientApp/src/components/modals/LocationDetailsForm/Steps/LocationImageStep.tsx
+++ src/TripPlanner.App/ClientApp/src/components/modals/LocationDetailsForm/Steps/LocationImageStep.tsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { useRecoilValue, useRecoilState } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import Button from '@mui/material/Button/Button';
 import { IStepValidator } from './Step';
 import LocationDto from '../../../../Common/Dto/LocationDto';
 import useLocationStepsStyles from './LocationStepsStyles';
 import { locationFormDataState, locationFormImageFile } from '../LocationDetailsFormState';
 import useStorageService from '../../../../Services/StorageService';
+import { isLoadingState, isLoadingTitleState } from '../../../../State/LocationsState';
 
 function LocationImageStepComponent() {
   const classes = useLocationStepsStyles();
-  const [imageFile, setImageFile] = useRecoilState(locationFormImageFile);
+  const setImageFile = useSetRecoilState(locationFormImageFile);
   const data = useRecoilValue(locationFormDataState);
   const [imageUrl, setImageUrl] = useState<string | null>();
   const storageService = useStorageService();
+  const setIsLoading = useSetRecoilState(isLoadingState);
+  const setIsLoadingTitle = useSetRecoilState(isLoadingTitleState);
 
   const setDefaultImage = () => setImageUrl(storageService.generateUrlToFile(data.image));
 
@@ -22,12 +25,15 @@ function LocationImageStepComponent() {
 
   const handleFileUpload = async (files: FileList | null) => {
     if (files && files[0]) {
-      // TODO add loading
+      setIsLoading(true);
+      setIsLoadingTitle('Loading image');
+
       setImageFile(files[0]);
 
       const reader = new FileReader();
       reader.onload = () => {
         setImageUrl(reader.result?.toString());
+        setIsLoading(false);
       };
 
       reader.readAsDataURL(files[0]);
@@ -41,7 +47,6 @@ function LocationImageStepComponent() {
       <form>
         <div className={classes.formRow}>
           <div className={classes.formItem}>
-            <div>{imageFile ?? imageFile ? imageFile.name : 'Empty'}</div>
             <label htmlFor="contained-button-file">
               <input
                 accept="image/*"
