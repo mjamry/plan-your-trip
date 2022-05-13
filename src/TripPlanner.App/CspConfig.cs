75be52ca7b7ccfe0b20d0a75b41d6df36b2267cf
mjamry <michal.jamry@gmail.com>
2022-05-13 12:59:22 +0200

    Improved csp policy including wikipedia urls

--- src/TripPlanner.App/CspConfig.cs
+++ src/TripPlanner.App/CspConfig.cs
@@ -14,12 +14,12 @@ namespace TripPlanner.App
             var config = conf.GetSection(nameof(Settings)).Get<Settings>();
             var cspHeader = "Content-Security-Policy";
             var cspValue = "default-src 'self'; " +
-                "style-src 'self' 'unsafe-inline' https://unpkg.com/leaflet@1.4.0/dist/ https://fonts.googleapis.com; " +
-                $"connect-src 'self' {config.AuthUrl} {config.ApiUrl} {config.StorageUrl} blob:; " +
-                "script-src 'self' www.google.com www.gstatic.com 'unsafe-eval' 'unsafe-inline'; " +
-                $"img-src 'self' {config.StorageUrl} https://a.basemaps.cartocdn.com/ https://b.basemaps.cartocdn.com/ https://c.basemaps.cartocdn.com/ https://unpkg.com/leaflet@1.4.0/dist/ data:; " +
+                "style-src 'self' 'unsafe-inline' https://unpkg.com/leaflet@1.4.0/dist/ https://fonts.googleapis.com/; " +
+                $"connect-src 'self' {config.AuthUrl} {config.ApiUrl} {config.StorageUrl} https://*.wikipedia.org/ https://*.wikimedia.org blob:; " +
+                "script-src 'self' www.google.com/ www.gstatic.com 'unsafe-eval' 'unsafe-inline'; " +
+                $"img-src 'self' {config.StorageUrl} https://*.wikimedia.org https://a.basemaps.cartocdn.com/ https://b.basemaps.cartocdn.com/ https://c.basemaps.cartocdn.com/ https://unpkg.com/leaflet@1.4.0/dist/ data:; " +
                 $"frame-src www.google.com {config.AuthUrl} blob:;" +
-                "font-src https://fonts.googleapis.com https://fonts.gstatic.com;";
+                "font-src https://fonts.googleapis.com/ https://fonts.gstatic.com/;";
 
             if (env.IsDevelopment())
             {
